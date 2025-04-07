48378fde599fd64c2796a22702cf325dc5a78ccb
Eric Koga <claireredfieldbr18@gmail.com>
2025-04-07 13:51:25 -0300

    Update repository

---
+++ Novato/Aula1/hello.c
@@ -0,0 +1,21 @@
+/*
+ * Esse é o nosso primeiro codigo
+ * Ele apresenta uma função principal 
+ * e um comando de saida na tela
+ * O include é a biblioteca e o int main é a função
+ * Toda função int retorna um valor no caso 0 para inteiro
+ * */
+#include <stdio.h> //Biblioteca padrao de entrada e saida
+
+//Função principal
+int main()
+//Inicio do programa
+{
+
+    //Comando de saida
+    printf("Ola, Mundo!\n");
+
+    //Retorna um valor inteiro
+    return 0;
+}
+//Fim do programa
