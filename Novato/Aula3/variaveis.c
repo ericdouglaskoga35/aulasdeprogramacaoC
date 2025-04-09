1581c4db2ba26cea69b32211123eed197473fb82
Eric Koga <claireredfieldbr18@gmail.com>
2025-04-09 07:14:27 -0300

    Atualizando...

---
+++ Novato/Aula3/variaveis.c
@@ -0,0 +1,31 @@
+/*
+ * Esse programa tem como intuito mostrar
+ * variaveis e os tipos de dados.
+ * */
+
+//Biblioteca padrao de entrada e saida
+#include <stdio.h>
+
+//Main função principal
+int main(){
+    /*
+     * Primeiro vem o tipo : nome da variavel
+     */
+    int notaA, notaB; //Variavel nota do tipo inteiro
+    float media; //Variavel media do tipo flutuante decimal
+    char nome[15] = "Eric"; //Variavel nome do tipo string texto
+    char letra = 'A'; //Variavel do tipo caractere
+    
+    //Estudar os tipos de dados que existem no C
+    
+    /*
+     * Regras: 
+     * Nao pode começar a variavel começando com numero
+     * Nao pode ser um identificador do codigo
+     * Napo de ter espaço, para isso use underline
+     * E case sensitive duas variaveis com maiusculo e minusculo vai existir no codigo
+     * Palavras reservadas que sao por exemplo identificador
+     * */
+
+    return 0;//Retorna uma saida do programa
+}
